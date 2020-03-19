0b00ad3e70123c1c6b6431dd521689aa7db0eea1
Wadih Ojeil <wadih.ojeil@gmail.com>
2020-03-19 18:29:29 -0400

    version 0.75 working on JS

--- Assets/script.js
+++ Assets/script.js
@@ -28,24 +28,21 @@ $(".saveBtn").on("click", function(){
 })
 
 var K = ["nIne","tEn","eLeven","tWelve","oNe","tWo","tHree","fOur","fIve"];
-var Area =["text9","text10","text11","text12","text1","text2","text3","text4","text5"];
+var Area =["#text9","#text10","#text11","#text12","#text1","#text2","#text3","#text4","#text5"];
 //$("#text9").text(localStorage.getItem("nIne"));
 
+//for loop will match the arrays above and link them via key method and then place them back on the screen.
 for (let i = 0; i < K.length; i++) {
-
+// cool gets all the setitems in the local storage.
 var cool = localStorage.getItem(K[i]);
-
-
 console.log(localStorage.getItem(K[i]));
+//if local storage is null then the loop will skip on printing back in text area. 
+if(cool != null) {
 
-if(cool === null) {
-    
+$(Area[i]).text(cool);
+
 }
 
-
-    
-    
-    
 }
    
     
