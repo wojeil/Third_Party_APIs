358151b0140cf01201c97cb75c2c93b4bf414e62
Wadih Ojeil <wadih.ojeil@gmail.com>
2020-03-19 00:27:07 -0400

    version 0.33 working on adding live time

--- Assets/script.js
+++ Assets/script.js
@@ -1,1 +1,16 @@
-$("#currentDay").moment().format('MMMM Do YYYY, h:mm:ss a');
+// var now = moment();
+// var time = moment($(e).attr('datetime'));
+// $("#currentDay").moment().format('MMMM Do YYYY, h:mm:ss a');
+
+// console.log(now);
+var now = moment();
+now.setattribute("id", "currentDay");
+now.innerhtml(moment());
+$('time').each(function(i, e) {
+    var time = moment($(e).attr('datetime'));
+
+    if(now.diff(time, 'days') <= 1) {
+        $(e).html('<span>' + time.from(now) + '</span>');
+    }
+});
+console.log(now);
