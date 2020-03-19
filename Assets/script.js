a582c59deeeafd07cc003f9485e7013ce2529ae1
Wadih Ojeil <wadih.ojeil@gmail.com>
2020-03-19 00:58:10 -0400

    version 0.4 working live time complete

--- Assets/script.js
+++ Assets/script.js
@@ -1,16 +1,3 @@
-// var now = moment();
-// var time = moment($(e).attr('datetime'));
-// $("#currentDay").moment().format('MMMM Do YYYY, h:mm:ss a');
 
-// console.log(now);
-var now = moment();
-now.setattribute("id", "currentDay");
-now.innerhtml(moment());
-$('time').each(function(i, e) {
-    var time = moment($(e).attr('datetime'));
-
-    if(now.diff(time, 'days') <= 1) {
-        $(e).html('<span>' + time.from(now) + '</span>');
-    }
-});
-console.log(now);
+//Time function
+$("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'))
