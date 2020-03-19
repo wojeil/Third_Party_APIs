dc1b4fe50415f86a4dad1da6cb9aa83cade79bd7
Wadih Ojeil <wadih.ojeil@gmail.com>
2020-03-19 10:00:25 -0400

    version 0.55 brain storming

--- Assets/script.js
+++ Assets/script.js
@@ -2,5 +2,11 @@
 //Setting Time 
 $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'))
 
+//setInterval(moment(), 1000),
+
 //Adding text to local Storage
 localStorage.setItem()
+
+var hour = today.get(Hours())\\
+if html < {}
+addClass ("blue")
