04cf96fd8a64f55abf81d1ddc1bce5f5dcec2b26
Wadih Ojeil <wadih.ojeil@gmail.com>
2020-03-19 22:42:18 -0400

    version 0.9 working on JS

--- Assets/script.js
+++ Assets/script.js
@@ -1,45 +1,39 @@
 $(document).ready(function () {
+//Global Arrays
+var K = ["nIne", "tEn", "eLeven", "tWelve", "oNe", "tWo", "tHree", "fOur", "fIve"];
+var Area = ["#text9", "#text10", "#text11", "#text12", "#text1", "#text2", "#text3", "#text4", "#text5"];
 
+//Setting Time 
+$("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'))
 
-    //Setting Time 
-    $("#currentDay").text(moment().format('MMMM Do YYYY, h:mm:ss a'))
+//setInterval(moment(), 1000),
 
-    //setInterval(moment(), 1000),
+  
 
-    //Adding text to local Storage
-    //localStorage.setItem()
+//Variables to help retrieve time in DOM
     var today = new Date();
     var hour = today.getHours();
-    
-    var plannerHours = $(".hour")
-console.log(plannerHours);
+    console.log(hour);
+//We use the loop 
+    for (var i = 0; i < Area.length; i++) {
+   
+    var data = $(Area[i]).attr("data-time");
+    parseInt(data)
 
-    //make a for loop  to run through the hours:
-
-    for (let j = 0; j < K.length; j++) {
-
-        if(hour == parseInt(K[j])) {
-
-            $(Area[j]).addClass(".present");
-        }
-        
-        
+    if(hour < data ) {
+        $(Area[i]).addClass("col-md-10 future description")
     }
 
+    if(hour == data ) {
+        $(Area[i]).addClass("col-md-10 present description")
+    }
 
-   // if (hour < parseInt(".hour"))
-
-
-
+    if(hour > data ) {
+        $(Area[i]).addClass("col-md-10 past description")
+    }
     
-    
-    console.log(hour);
-    //if html < {}
-    //addClass ("blue")
-
-
-
-
+}
+  
 
 //adding an event listener to text content:
 
@@ -50,12 +44,11 @@ console.log(plannerHours);
         //saves it in local storage.
         localStorage.setItem(time, value);
     })
-    //Global Arrays
-    var K = ["nIne", "tEn", "eLeven", "tWelve", "oNe", "tWo", "tHree", "fOur", "fIve"];
-    var Area = ["#text9", "#text10", "#text11", "#text12", "#text1", "#text2", "#text3", "#text4", "#text5"];
-    //$("#text9").text(localStorage.getItem("nIne"));
+  
+   
+//$("#text9").text(localStorage.getItem("nIne"));
     
-    //for loop will match the arrays above and link them via key method and then place them back on the screen.
+//for loop will match the arrays above and link them via key method and then place them back on the screen.
     for (let i = 0; i < K.length; i++) {
         // cool gets all the setitems in the local storage.
         var cool = localStorage.getItem(K[i]);
@@ -66,9 +59,6 @@ console.log(plannerHours);
             $(Area[i]).text(cool);
         }
     }
-
-
-
     console.log(localStorage.length);
 
 });
